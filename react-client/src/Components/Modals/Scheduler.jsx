3be137b5ebc1a8fa01093e614d84be6e5065450e
Abel Romer <abel.romer@questu.ca>
2018-10-26 22:08:30 -0700

    Add pref selection for individuals.

--- react-client/src/Components/Modals/Scheduler.jsx
+++ react-client/src/Components/Modals/Scheduler.jsx
@@ -39,9 +39,70 @@ class Weekday extends Component {
 }
 
 class DayHeader extends Component {
+    constructor(props) {
+        super(props);
+        this.state =
+            {
+                showPrefs: false,
+                status: {
+                    "Ann": "unselected",
+                    "Lily": "unselected",
+                    "Tim": "unselected",
+                    "Jess": "unselected",
+                    "Sara": "unselected",
+                    "Jill": "unselected",
+                    "Dan": "unselected",
+                    "Kev": "unselected"
+                },
+                name: this.props.name
+            };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.highlightData == null) {
+            return null;
+        }
+        else if (this.props.highlightData == null) {
+            this.setState((state) => {
+                return (
+                    {
+                        showPrefs: nextProps.highlightData.highlightPrefs
+                    }
+                )
+            });
+        }
+        else if (nextProps.highlightData.highlightPrefs !== this.props.highlightData.highlightPrefs) {
+            this.setState((state) => {
+                return (
+                    {
+                        showPrefs: nextProps.highlightData.highlightPrefs
+                    }
+                )
+            });
+        }
+    }
+
+    changeDayPref = () => {
+        let currentName = this.props.highlightData.name;
+        let statusDict = this.state.status;
+        statusDict[currentName] = (this.state.status[currentName] === "available" ? "unavailable" : "available")
+        this.setState((state) => {
+            return (
+                {
+                    status: statusDict
+                }
+            )
+        });
+    }
+
     render() {
+        let showPrefs = this.state.showPrefs;
+        let status = (this.props.highlightData ? this.state.status[this.props.highlightData.name] : "unselected");
+
         return (
-            <div className={"day-name"} ref={this.dayName}>{this.props.name}</div>
+            <div className={"day-name " + (showPrefs ? status : "") } onClick={this.changeDayPref}>{this.props.name}
+                {this.props.name}
+            </div>
         )
     }
 }
@@ -57,13 +118,13 @@ class Calendar extends Component {
 
         return (
             <div className={"schedule-calendar"}>
-                <DayHeader name="Sunday" />
-                <DayHeader name="Monday" />
-                <DayHeader name="Tuesday" />
-                <DayHeader name="Wednesday" />
-                <DayHeader name="Thursday" />
-                <DayHeader name="Friday" />
-                <DayHeader name="Saturday" />
+                <DayHeader name="Sunday" highlightData={this.props.dataFromPrefs} />
+                <DayHeader name="Monday" highlightData={this.props.dataFromPrefs} />
+                <DayHeader name="Tuesday" highlightData={this.props.dataFromPrefs} />
+                <DayHeader name="Wednesday" highlightData={this.props.dataFromPrefs} />
+                <DayHeader name="Thursday" highlightData={this.props.dataFromPrefs} />
+                <DayHeader name="Friday" highlightData={this.props.dataFromPrefs} />
+                <DayHeader name="Saturday" highlightData={this.props.dataFromPrefs} />
                 <Weekend />
                 <Weekday />
                 <Weekday />
